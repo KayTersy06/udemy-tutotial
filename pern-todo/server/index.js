a104e7ec3d3a7e2d2704b82f6499cb758e6b38c8
KayTersy06 <xolaniskosana06@gmail.com>
2025-11-18 13:50:24 +0200

    2nd commit

--- pern-todo/server/index.js
+++ pern-todo/server/index.js
@@ -10,13 +10,19 @@ app.use(express.json()); //req.body
 //ROUTES//
 
 //create a todo
-app.post("/todos", (req,res) => {
+app.post("/todos", async(req,res) => {
     try{
-        
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *", 
+            [description]
+        );
+
+        res.json(newTodo.rows[0]);
     }catch(err){
         console.error(err.message);
     }
-})
+});
 
 //get all todos
 
