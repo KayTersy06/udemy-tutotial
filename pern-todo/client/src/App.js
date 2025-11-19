ac2b0aea08abad23ddbe03f4cb4823a831199fc0
KayTersy06 <xolaniskosana06@gmail.com>
2025-11-19 11:33:08 +0200

    List Todos component init commit

--- pern-todo/client/src/App.js
+++ pern-todo/client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 // components
 import InputTodo from "./components/InputTodo.js";
+import ListTodos from "./components/ListTodos.js";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   );
