a1cc325a3b8cecd32ae83f5bc5a928b05201f368
KayTersy06 <xolaniskosana06@gmail.com>
2025-11-19 11:09:47 +0200

    Input Todo component

--- pern-todo/client/src/App.js
+++ pern-todo/client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+// components
+import InputTodo from "./components/InputTodo.js";
+
 function App() {
   return (
     <Fragment>
-      
+      <div className="container">
+        <InputTodo />
+      </div>
     </Fragment>
   );
 }
